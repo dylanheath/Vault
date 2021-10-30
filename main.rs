94167b3a87bce108b5ed78d772354377d89fd319
Dylan <dylanheathcote2004@gmail.com>
2021-10-31 00:37:01 +0100

    spinning dots when loading  users

--- main.rs
+++ main.rs
@@ -40,19 +40,25 @@ struct User {
 
 async fn find_user(coll: mongodb::Collection::<User>) -> mongodb::error::Result<()> {
     
+    println!("[*] enter username");
     let mut username = String::new();
     io::stdin().read_line(&mut username).expect("Failed to get input");
+    
+    let handle = SpinnerBuilder::new().spinner(&DOTS).text("getting user").start();
 
     let mut cursor = coll.find(doc! {"name": username}, None).await?;
     while let Some(user) = cursor.try_next().await? {
         println!("{:?}", user);
     }
+    std::thread::sleep(std::time::Duration::from_secs(3));
+    handle.done();
+
     Ok(())
 }
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
-    let client = Client::with_uri_str("mongodb+srv://main:1234@cluster0.h7ieh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").await?;
+    let client = Client::with_uri_str("mongodb+srv://Admin:1234@cluster0.h7ieh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").await?;
     let db = client.database("myFirstDatabase");
 
     let coll = db.collection::<User>("User");
