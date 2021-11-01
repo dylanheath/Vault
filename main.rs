fd0fa89a7afd1aa93736b928627a2c52fa00812f
Dylan <60988210+AcenGit@users.noreply.github.com>
2021-11-01 17:04:43 +0000

    Update main.rs

--- main.rs
+++ main.rs
@@ -51,46 +51,7 @@ impl fmt::Display for User{
    }
 }
 
-fn exit(currentUser: User) {
 
-
-}
-
-fn search(currentUser: User) {
-
-    
-}
-
-fn view(currentUser: User) {
-    
-}
-
-fn add(currentUser: User) {
-    
-}
-
-//get user input and then pass currentUser to a function which is picked
-fn menu(currentUser: User) {
-    let mut menu_option = String::new();
-    io::stdin().read_line(&mut menu_option).expect("Failed to get line");
-
-    if menu_option == "1" {
-        add(currentUser)
-    }
-
-    if menu_option == "2" {
-        view(currentUser)
-    }
-    
-    if menu_option == "3" {
-        search(currentUser)
-    }
-    
-    if menu_option == "4" {
-        exit(currentUser)
-    } 
- 
-}
 
 
 async fn find_user(coll: mongodb::Collection::<User>) -> mongodb::error::Result<()> {
